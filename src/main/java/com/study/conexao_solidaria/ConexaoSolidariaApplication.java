063e1e1c7f443aff76170364796f06b4176f7f8e
Luiz <luizfernandodemoura.2013@gmail.com>
2024-08-04 15:38:23 -0300

    Creating project

---
+++ src/main/java/com/study/conexao_solidaria/ConexaoSolidariaApplication.java
@@ -0,0 +1,13 @@
+package com.study.conexao_solidaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexaoSolidariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexaoSolidariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/conexao_solidaria/ConexaoSolidariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.conexao_solidaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConexaoSolidariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
