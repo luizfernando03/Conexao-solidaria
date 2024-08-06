ffe3e54fd650afc7ccadc94ee208496fcd642fac
Luiz <luizfernandodemoura.2013@gmail.com>
2024-08-06 00:02:39 -0300

    Creating exceptions StandardError

---
+++ src/main/java/com/study/conexao_solidaria/exceptions/StandardError.java
@@ -0,0 +1,22 @@
+package com.study.conexao_solidaria.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StandardError {
+
+    private static final long serialVersionUID = 1L;
+
+    private Instant timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+
+}
